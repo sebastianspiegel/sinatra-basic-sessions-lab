97b9e2bc0fbb493b894b62a9b06877b2e74dd7a4
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-01 15:25:53 -0500

    commit tree c63dbc951331280a660047c328e49f72969004b2

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "ftr516"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @sessions = session 
+        erb :checkout 
+    end
 end
